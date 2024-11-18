0183756d99eef4bdf4946e1612b935619f6abcb8
saipradeep baggam <animeoceanflix@gmail.com>
2024-11-18 09:58:49 +0530

    add selection sort

--- Leetscode/sorting algorithms/selection_sort.py
+++ Leetscode/sorting algorithms/selection_sort.py
@@ -4,18 +4,35 @@
 def selection_sort(arr):
     n = len(arr)
 
-    for i in range(0, n - 1):
+    for i in range(n):
         min_index = i
 
-        for j in range(i + 1, n):
+        for j in range(min_index + 1, n):
             if arr[j] < arr[min_index]:
                 min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
+        if i != min_index:
+            arr[i], arr[min_index] = arr[min_index], arr[i]
 
     return arr
 
 
-my_list = [12, 25, 11, 34, 90, 22]
+# approach 2 :
+
+# def selection_sort(arr):
+#     n = len(arr)
+
+#     for i in range(0, n - 1):
+#         min_index = i
+
+#         for j in range(i + 1, n):
+#             if arr[j] < arr[min_index]:
+#                 min_index = j
+#         arr[i], arr[min_index] = arr[min_index], arr[i]
+
+#     return arr
+
+
+my_list = [12, 255, 11, 34, 90, 222, 2, 4]
 
 result = selection_sort(my_list)
 
